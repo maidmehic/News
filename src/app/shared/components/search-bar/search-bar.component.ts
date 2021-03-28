6f5dc0c6a4ea7beb4dcab36eeebb02e9f6dfcb29
Maid Mehic <maid.mehic@compusight.com>
2021-03-28 09:38:46 +0200

    Improve searchbar

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class SearchBarComponent implements OnInit {
 
   @Input() searchTerm: string;
+  @Input() placeholder: string;
   @Output() onSearchEvent = new EventEmitter<string>();
   constructor() { }
 
